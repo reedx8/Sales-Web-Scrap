1b050425518902253acdd2ef3b75dea65f7b448e
Xavier Reed <xreed18@gmail.com>
2024-01-04 15:03:56 -0800

    uber's grabbing sales done

--- uber.py
+++ uber.py
@@ -3,23 +3,29 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Chrome, ChromeOptions
-from time import sleep # but not used here
-from selenium.webdriver.common.action_chains import ActionChains # dont always need, not ussed here
+from time import sleep 
+from selenium.webdriver.common.action_chains import ActionChains 
 from sheet import send_data
 from dotenv import load_dotenv
 
 load_dotenv()
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(5) # Global setting that sets driver to wait a max of x seconds to find each requested element in DOM tree if they are not immediately available in DOM already
+driver.implicitly_wait(5) 
 actions = ActionChains(driver)
 options = ChromeOptions()
-options.add_argument("window-size=1200x600") # sets window size to remove variables (avoids mobile view)
+options.add_argument("window-size=1200x600") 
 
 login_url = os.getenv("UBER_LOGIN_URL")
-# target_url = 'https://example.com/dashboard/' 
 username = os.getenv("UBER_USERNAME")
 password = os.getenv("UBER_PW")
+
+all_stores = [
+    "AVA Roasteria (SW Hall Blvd)",
+    "Ava Roasteria (Barrows Road)",
+    "AVA Roasteria (Lake Oswego)",
+    "Ava Roasteria (Orenco Station)"
+]
 
 # Step 1: Handle Login
 driver.get(login_url)
@@ -48,7 +54,21 @@ sleep(10)
 
 
 # Step 2: Grab live sales from each store
-# ...
+driver.switch_to.window(driver.window_handles[0])
+sleep(5) # needed, else stale element error on menu_btn
+for store in all_stores:
+    menu_btn = driver.find_element(By.XPATH, "//*[@id='wrapper']/div[1]/div[2]/div[2]/div[1]/div/div[1]/button")
+    actions.move_to_element(menu_btn).click(menu_btn).perform()
+    sleep(3)
+    store_link = driver.find_element(By.XPATH, f"//p[text()='{store}']")
+    sleep(1)
+    actions.move_to_element(store_link).click(store_link).perform()
+    sleep(10)
+    sales = driver.find_element(By.XPATH, "//h5[@data-baseweb='typo-headingsmall']").text
+    print(store, ": ", sales)
+
+
+
 
 # Step 3: Quit selenium properly, and exit program. Done. 
 driver.quit()
