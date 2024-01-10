000d5710857fedc1a37a070cb2c1f2a8173c848d
zarrinmonirzadeh <z.monirzadeh@gmail.com>
2024-01-10 12:47:36 -0800

    add login page and automatically login in Grubhub

--- grubhub.py
+++ grubhub.py
@@ -1,1 +1,126 @@
 # Zarrin: add your code below
+import os
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Chrome, ChromeOptions
+from time import sleep 
+from selenium.webdriver.common.action_chains import ActionChains 
+from sheet import send_data
+from dotenv import load_dotenv
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+load_dotenv()
+
+# if sys.argv[1] == 'x3':
+#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+# else:
+#     driver = webdriver.Chrome()
+# driver.implicitly_wait(5) 
+# actions = ActionChains(driver)
+options = ChromeOptions()
+options.add_argument("window-size=1200x600") 
+
+login_url = os.getenv("GRUBHUB_LOGIN_URL")
+username = os.getenv("GRUBHUB_USERNAME")
+password = os.getenv("GRUBHUB_PW")
+
+hall = "Ava Roasteria - SW Hall Blvd"
+barrows = "Ava Roasteria - SW Barrows Rd"
+kruse = "Ava Roasteria - Meadows Rd"
+orenco = "Ava Roasteria - NE Orenco Station Loop"
+
+all_stores = [
+    hall,
+    barrows,
+    kruse,
+    orenco
+]
+
+all_sales = {
+    "Hall": 0,
+    "Barrows": 0,
+    "Kruse": 0, 
+    "Orenco": 0
+}
+
+def run_grubhub():
+    print("\nRunning Grubhub...")
+
+    # Step 0: x3 path option for windows, else run for macs
+    if sys.argv[1] == 'x3':
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    else:
+        driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
+    actions = ActionChains(driver)
+
+    # Step 1: Handle Login
+    driver.get(login_url)
+
+    # Fetch username and password elements
+    username_field = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "gfr-login-authentication-username"))
+    )
+    password_field = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "gfr-login-authentication-password"))
+    )
+
+    # Input login credentials
+    username_field.send_keys(username)
+    password_field.send_keys(password)
+
+    # Click the "Show" button to reveal the password (if needed)
+    show_password_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//h6[text()='Show']"))
+    )
+    show_password_button.click()
+
+    # Submit the login form
+    login_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//span[text()='Sign in']"))
+    )
+    login_button.click()
+
+
+ 
+       
+
+   # Step 2: Grab live sales from each store
+    driver.switch_to.window(driver.window_handles[0])
+    sleep(5)  # needed, else stale element error on menu_btn
+    for store in all_stores:
+        menu_btn = driver.find_element(By.XPATH, "//*[@id='wrapper']/div[1]/div[2]/div[2]/div[1]/div/div[1]/button")
+        actions.move_to_element(menu_btn).click(menu_btn).perform()
+        sleep(5)  # Needed. sleep(3) seconds wasn't always long enough wait for menu+element to load
+        store_link = driver.find_element(By.XPATH, f"//p[text()='{store}']")
+        sleep(1)
+        actions.move_to_element(store_link).click(store_link).perform()
+        sleep(10)
+
+        sales = driver.find_element(By.XPATH, "//h5[@data-baseweb='typo-headingsmall']").text
+        print(store, ": ", sales)
+
+        if store == hall:
+            all_sales["Hall"] = sales
+        elif store == barrows:
+            all_sales["Barrows"] = sales
+        elif store == kruse:
+            all_sales["Kruse"] = sales
+        elif store == orenco:
+            all_sales["Orenco"] = sales
+
+    # Step 3: Send live sales data to spreadsheet
+    send_data(all_sales, "grubhub")
+
+    # Step 4: Quit selenium properly, and exit program. Done. 
+    driver.quit()
+
+# Run the function if this script is executed
+if __name__ == "__main__":
+    run_grubhub()
--- sheet.py
+++ sheet.py
@@ -1,10 +1,11 @@
 '''
 TODO:
-- Grab todays date, output to correct column in sheet
+- Grab today's date, output to the correct column in the sheet
 '''
 import sys
 import pygsheets
 import pandas as pd
+from datetime import datetime
 
 # authorization: replace with your own path option. The path is the file path to where your google_api_cred.json file is located on your machine.
 try:
@@ -17,6 +18,10 @@ try:
     elif (sys.argv[1] == 'x3'):
         # path option 3 -- Xavier's asus windows machine
         path = 'C:\\Users\\reedx\\Code\\Sales-Web-Scrap\\google_api_cred.json'
+    elif (sys.argv[1] == 'z'):
+        # path option 4 -- zarrins office computer
+        path = "D:\\Ava\\Sales-Web-Scrap\\google_api_cred.json"
+
 except Exception as error:
     print("\n*************************")
     print("Exception: Missing command line argument (see sheet.py). Please try again.\n")
@@ -44,28 +49,36 @@ def send_data(sales_data, platform):
 
     # Create a column with Doordash/Revel column title and its sales_data
     # Ensure to insert sales data in the following order: Hall, Barrows, Kruse, then Orenco
-    if (platform.lower() == "doordash"):
+    if platform.lower() == "doordash":
         df['Doordash'] = [
             sales_data['Hall'],
             sales_data['Barrows'],
             sales_data['Kruse'],
             sales_data['Orenco']
         ]
-        # update sheet with df, starting at cell C1 (doordash)
-        sh.set_dataframe(df,(1,3)) # set_dataframe(df, (row, column))
-    elif (platform.lower() == "revel"):
+        # Update sheet with df, starting at cell C1 (Doordash)
+        sh.set_dataframe(df, (1, 3))  # set_dataframe(df, (row, column))
+    elif platform.lower() == "revel":
         df['Revel'] = [
             sales_data['Hall'],
             sales_data['Barrows'],
             sales_data['Kruse'],
             sales_data['Orenco']
         ]
-        sh.set_dataframe(df, (1,2))
-    elif (platform.lower() == "uber"):
+        sh.set_dataframe(df, (1, 2))
+    elif platform.lower() == "uber":
         df["Uber Eats"] = [
             sales_data['Hall'],
             sales_data['Barrows'],
             sales_data['Kruse'],
             sales_data['Orenco']
         ]
-        sh.set_dataframe(df, (1,5))
+        sh.set_dataframe(df, (1, 5))
+    elif platform.lower() == "grubhub":
+        df["Grubhub"] = [
+            sales_data['Hall'],
+            sales_data['Barrows'],
+            sales_data['Kruse'],
+            sales_data['Orenco']
+        ]
+        sh.set_dataframe(df, (1, 4))
